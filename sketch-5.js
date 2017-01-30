02c187b0b75df5bd610c5943e8db8695e3da9898
selinalyc <yliu3@cca.edu>
2017-01-29 20:18:32 -0800

    Create sketch-5.js

---
+++ sketch-5.js
@@ -0,0 +1,20 @@
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+var x=100;
+var y=100;
+var direction=3;
+
+function draw() { 
+
+  background(220);
+	ellipse(x,y,20,20); 
+	x=x+direction;
+if(x>390||x<10){
+		direction*=-1;
+    //what is * for?
+    //why it is -1 instead of 1?
+	}	
+	
+}
